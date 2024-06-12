4528a2b6d1648cff67c4673940b9636aa306070c
Lolom <lorenzoaoulini@gmail.com>
2024-06-12 15:11:41 +0200

    deploy

--- apps/server/build.js
+++
@@ -1,9 +0,0 @@
-import * as esbuild from "esbuild";
-
-esbuild.build({
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  platform: "node",
-  outfile: "dist/index.js",
-  loader: { ".node": "file" },
-});
