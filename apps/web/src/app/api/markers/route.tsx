fb04f9328feb777d12b47920abce5eaeef09d61f
Lolom <lorenzoaoulini@gmail.com>
2024-06-14 12:47:02 +0200

    dynamic routes

--- apps/web/src/app/api/markers/route.tsx
+++ apps/web/src/app/api/markers/route.tsx
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getAllMarkers } from "@repo/db";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
   return NextResponse.json(await getAllMarkers());
 }
