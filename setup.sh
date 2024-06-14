1feed124f03bbe4c1b52db978f18f394061f91e1
Lolom <lorenzoaoulini@gmail.com>
2024-06-14 16:02:33 +0200

    switch to npm

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 
 cp .env apps/web/.env 2> /dev/null || :
-apt-get update 
-apt-get install libuuid
-apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+# apt-get update 
+# apt-get install libuuid 
+# apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
