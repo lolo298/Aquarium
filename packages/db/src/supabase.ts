04d83d1cf8299e5e4c578f3765c1385342df80b3
Lolom <lorenzoaoulini@gmail.com>
2024-06-14 12:58:08 +0200

    log

--- packages/db/src/supabase.ts
+++ packages/db/src/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("ENV: ", process.env);
+
 const supabaseUrl = "https://ofnectvdmnyxxznhaagk.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY ?? "";
 export const supabase = createClient(supabaseUrl, supabaseKey);
