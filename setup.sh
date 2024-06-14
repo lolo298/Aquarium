215ac0012ff02402e73e6082182be0bdcf6f3a46
Lolom <lorenzoaoulini@gmail.com>
2024-06-14 16:13:47 +0200

    build canvas

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 
 cp .env apps/web/.env 2> /dev/null || :
-# apt-get update 
+apt-get update 
+apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 # apt-get install libuuid 
 # apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
