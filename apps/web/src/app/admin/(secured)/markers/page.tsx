aa87d0d5a459fc0ccd2a0ef99faa7504bc4d7a78
Lolom <lorenzoaoulini@gmail.com>
2024-06-20 17:09:54 +0200

    fix build

--- apps/web/src/app/admin/(secured)/markers/page.tsx
+++ apps/web/src/app/admin/(secured)/markers/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import DataTable from "./DataTable";
-import { getQueryClient } from "@/lib/query";
 
 function Markers() {
-  const queryClient = getQueryClient();
+  const queryClient = new QueryClient();
 
   return (
     <>
